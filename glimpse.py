ae6c5a427ec6feda0e78511c4f2b9698a2085c95
Ignacio Tartavull <tartavull@gmail.com>
2016-10-05 08:31:19 -0400

    New organization of code

--- glimpse.py
+++ glimpse.py
@@ -4,7 +4,7 @@ from __future__ import print_function
 
 import tensorflow as tf
 
-from utils import weight_variable, bias_variable
+from utils import weight_variable, bias_variable, loglikelihood
 
 
 class GlimpseNet(object):
@@ -19,28 +19,23 @@ class GlimpseNet(object):
     self.num_channels = config.num_channels
     self.sensor_size = config.sensor_size
     self.win_size = config.win_size
-    self.minRadius = config.minRadius
-    self.depth = config.depth
-
-    self.hg_size = config.hg_size
-    self.hl_size = config.hl_size
-    self.g_size = config.g_size
-    self.loc_dim = config.loc_dim
 
     self.images_ph = images_ph
+    self.init_weights(config)
 
-    self.init_weights()
-
-  def init_weights(self):
+  def init_weights(self, config):
     """ Initialize all the trainable weights."""
-    self.w_g0 = weight_variable((self.sensor_size, self.hg_size))
-    self.b_g0 = bias_variable((self.hg_size,))
-    self.w_l0 = weight_variable((self.loc_dim, self.hl_size))
-    self.b_l0 = bias_variable((self.hl_size,))
-    self.w_g1 = weight_variable((self.hg_size, self.g_size))
-    self.b_g1 = bias_variable((self.g_size,))
-    self.w_l1 = weight_variable((self.hl_size, self.g_size))
-    self.b_l1 = weight_variable((self.g_size,))
+    self.w_g0 = weight_variable((config.sensor_size, config.hg_size))
+    self.b_g0 = bias_variable((config.hg_size,))
+
+    self.w_l0 = weight_variable((config.loc_dim, config.hl_size))
+    self.b_l0 = bias_variable((config.hl_size,))
+
+    self.w_g1 = weight_variable((config.hg_size, config.g_size))
+    self.b_g1 = bias_variable((config.g_size,))
+
+    self.w_l1 = weight_variable((config.hl_size, config.g_size))
+    self.b_l1 = weight_variable((config.g_size,))
 
   def get_glimpse(self, loc):
     """Take glimpse on the original images."""
@@ -73,7 +68,6 @@ class LocNet(object):
   Take output from other network and produce and sample the next location.
 
   """
-
   def __init__(self, config):
     self.loc_dim = config.loc_dim
     self.input_dim = config.cell_output_size
@@ -105,3 +99,92 @@ class LocNet(object):
   @sampling.setter
   def sampling(self, sampling):
     self._sampling = sampling
+
+
+
+class CoreNet(object):
+
+  def __init__(self, config, mnist):
+    self.loc_mean_arr = []
+    self.sampled_loc_arr = []
+    self.create_placeholders(config)
+    self.create_auxiliary_networks(config)
+
+    # 0.0 is the center of the image while -1 and 1 are the extrems
+    # when taking glimpses
+    init_loc = tf.random_uniform((self.n_examples, 2), minval=-1, maxval=1)
+    init_glimpse = self.gl(init_loc)
+
+    # Core network.
+    lstm_cell = tf.nn.rnn_cell.LSTMCell(config.cell_size, state_is_tuple=True)
+    init_state = lstm_cell.zero_state(self.n_examples, tf.float32)
+    inputs = [init_glimpse]
+    inputs.extend([0] * (config.num_glimpses))
+    outputs, _ = tf.nn.seq2seq.rnn_decoder(
+      inputs, init_state, lstm_cell, loop_function=self.get_next_input)
+
+
+    # Time independent baselines
+    with tf.variable_scope('baseline'):
+      w_baseline = weight_variable((config.cell_output_size, 1))
+      b_baseline = bias_variable((1,))
+    baselines = []
+    for t, output in enumerate(outputs[1:]):
+      baseline_t = tf.nn.xw_plus_b(output, w_baseline, b_baseline)
+      baseline_t = tf.squeeze(baseline_t)
+      baselines.append(baseline_t)
+    baselines = tf.pack(baselines)  # [timesteps, batch_sz]
+    baselines = tf.transpose(baselines)  # [batch_sz, timesteps]
+
+    # Take the last step only.
+    output = outputs[-1]
+    # Build classification network.
+    with tf.variable_scope('cls'):
+      w_logit = weight_variable((config.cell_output_size, config.num_classes))
+      b_logit = bias_variable((config.num_classes,))
+    logits = tf.nn.xw_plus_b(output, w_logit, b_logit)
+    self.softmax = tf.nn.softmax(logits)
+
+    # cross-entropy.
+    self.xent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, self.labels_ph)
+    self.xent = tf.reduce_mean(self.xent)
+    pred_labels = tf.argmax(logits, 1)
+    # 0/1 reward.
+    reward = tf.cast(tf.equal(pred_labels, self.labels_ph), tf.float32)
+    rewards = tf.expand_dims(reward, 1)  # [batch_sz, 1]
+    rewards = tf.tile(rewards, (1, config.num_glimpses))  # [batch_sz, timesteps]
+    logll = loglikelihood(self.loc_mean_arr, self.sampled_loc_arr, config.loc_std)
+    self.advs = rewards - tf.stop_gradient(baselines)
+    self.logllratio = tf.reduce_mean(logll * self.advs)
+    self.reward = tf.reduce_mean(reward)
+
+    self.baselines_mse = tf.reduce_mean(tf.square((rewards - baselines)))
+    self.var_list = tf.trainable_variables()
+    # hybrid loss
+    self.loss = -self.logllratio + self.xent + self.baselines_mse  # `-` for minimize
+    grads = tf.gradients(self.loss, self.var_list)
+    self.grads, _ = tf.clip_by_global_norm(grads, config.max_grad_norm)
+
+ 
+
+  def get_next_input(self, output, i):
+    loc, loc_mean = self.loc_net(output)
+    gl_next = self.gl(loc)
+    self.loc_mean_arr.append(loc_mean)
+    self.sampled_loc_arr.append(loc)
+    return gl_next
+
+  def create_placeholders(self, config):
+    self.images_ph = tf.placeholder(tf.float32,
+                               [None, config.original_size * config.original_size *
+                                config.num_channels])
+    self.n_examples = tf.shape(self.images_ph)[0] # number of examples
+    
+    self.labels_ph = tf.placeholder(tf.int64, [None])
+
+
+  def create_auxiliary_networks(self, config):
+    with tf.variable_scope('glimpse_net'):
+      self.gl = GlimpseNet(config, self.images_ph)
+    with tf.variable_scope('loc_net'):
+      self.loc_net = LocNet(config)
--- ram.py
+++ ram.py
@@ -8,97 +8,19 @@ import logging
 import tensorflow as tf
 import numpy as np
 
-from glimpse import GlimpseNet, LocNet
-from utils import weight_variable, bias_variable, loglikelihood
+from glimpse import GlimpseNet, LocNet, CoreNet
 from config import Config
 
 from tensorflow.examples.tutorials.mnist import input_data
 
 logging.getLogger().setLevel(logging.INFO)
 
-rnn_cell = tf.nn.rnn_cell
-seq2seq = tf.nn.seq2seq
 
 mnist = input_data.read_data_sets('MNIST_data', one_hot=False)
 
 config = Config()
 n_steps = config.step
-
-loc_mean_arr = []
-sampled_loc_arr = []
-
-
-def get_next_input(output, i):
-  loc, loc_mean = loc_net(output)
-  gl_next = gl(loc)
-  loc_mean_arr.append(loc_mean)
-  sampled_loc_arr.append(loc)
-  return gl_next
-
-# placeholders
-images_ph = tf.placeholder(tf.float32,
-                           [None, config.original_size * config.original_size *
-                            config.num_channels])
-labels_ph = tf.placeholder(tf.int64, [None])
-
-# Build the aux nets.
-with tf.variable_scope('glimpse_net'):
-  gl = GlimpseNet(config, images_ph)
-with tf.variable_scope('loc_net'):
-  loc_net = LocNet(config)
-
-# number of examples
-N = tf.shape(images_ph)[0]
-init_loc = tf.random_uniform((N, 2), minval=-1, maxval=1)
-init_glimpse = gl(init_loc)
-# Core network.
-lstm_cell = rnn_cell.LSTMCell(config.cell_size, state_is_tuple=True)
-init_state = lstm_cell.zero_state(N, tf.float32)
-inputs = [init_glimpse]
-inputs.extend([0] * (config.num_glimpses))
-outputs, _ = seq2seq.rnn_decoder(
-    inputs, init_state, lstm_cell, loop_function=get_next_input)
-
-# Time independent baselines
-with tf.variable_scope('baseline'):
-  w_baseline = weight_variable((config.cell_output_size, 1))
-  b_baseline = bias_variable((1,))
-baselines = []
-for t, output in enumerate(outputs[1:]):
-  baseline_t = tf.nn.xw_plus_b(output, w_baseline, b_baseline)
-  baseline_t = tf.squeeze(baseline_t)
-  baselines.append(baseline_t)
-baselines = tf.pack(baselines)  # [timesteps, batch_sz]
-baselines = tf.transpose(baselines)  # [batch_sz, timesteps]
-
-# Take the last step only.
-output = outputs[-1]
-# Build classification network.
-with tf.variable_scope('cls'):
-  w_logit = weight_variable((config.cell_output_size, config.num_classes))
-  b_logit = bias_variable((config.num_classes,))
-logits = tf.nn.xw_plus_b(output, w_logit, b_logit)
-softmax = tf.nn.softmax(logits)
-
-# cross-entropy.
-xent = tf.nn.sparse_softmax_cross_entropy_with_logits(logits, labels_ph)
-xent = tf.reduce_mean(xent)
-pred_labels = tf.argmax(logits, 1)
-# 0/1 reward.
-reward = tf.cast(tf.equal(pred_labels, labels_ph), tf.float32)
-rewards = tf.expand_dims(reward, 1)  # [batch_sz, 1]
-rewards = tf.tile(rewards, (1, config.num_glimpses))  # [batch_sz, timesteps]
-logll = loglikelihood(loc_mean_arr, sampled_loc_arr, config.loc_std)
-advs = rewards - tf.stop_gradient(baselines)
-logllratio = tf.reduce_mean(logll * advs)
-reward = tf.reduce_mean(reward)
-
-baselines_mse = tf.reduce_mean(tf.square((rewards - baselines)))
-var_list = tf.trainable_variables()
-# hybrid loss
-loss = -logllratio + xent + baselines_mse  # `-` for minimize
-grads = tf.gradients(loss, var_list)
-grads, _ = tf.clip_by_global_norm(grads, config.max_grad_norm)
+cn = CoreNet(config, mnist)
 
 # learning rate
 global_step = tf.get_variable(
@@ -114,7 +36,8 @@ learning_rate = tf.train.exponential_decay(
     staircase=True)
 learning_rate = tf.maximum(learning_rate, config.lr_min)
 opt = tf.train.AdamOptimizer(learning_rate)
-train_op = opt.apply_gradients(zip(grads, var_list), global_step=global_step)
+train_op = opt.apply_gradients(zip(cn.grads, cn.var_list), global_step=global_step)
+
 
 with tf.Session() as sess:
   sess.run(tf.initialize_all_variables())
@@ -123,14 +46,14 @@ with tf.Session() as sess:
     # duplicate M times, see Eqn (2)
     images = np.tile(images, [config.M, 1])
     labels = np.tile(labels, [config.M])
-    loc_net.samping = True
+    cn.loc_net.samping = True
     adv_val, baselines_mse_val, xent_val, logllratio_val, \
         reward_val, loss_val, lr_val, _ = sess.run(
-            [advs, baselines_mse, xent, logllratio,
-             reward, loss, learning_rate, train_op],
+            [cn.advs, cn.baselines_mse, cn.xent, cn.logllratio,
+             cn.reward, cn.loss, learning_rate, train_op],
             feed_dict={
-                images_ph: images,
-                labels_ph: labels
+                cn.images_ph: images,
+                cn.labels_ph: labels
             })
     if i and i % 100 == 0:
       logging.info('step {}: lr = {:3.6f}'.format(i, lr_val))
@@ -153,10 +76,10 @@ with tf.Session() as sess:
           # Duplicate M times
           images = np.tile(images, [config.M, 1])
           labels = np.tile(labels, [config.M])
-          softmax_val = sess.run(softmax,
+          softmax_val = sess.run(cn.softmax,
                                  feed_dict={
-                                     images_ph: images,
-                                     labels_ph: labels
+                                     cn.images_ph: images,
+                                     cn.labels_ph: labels
                                  })
           softmax_val = np.reshape(softmax_val,
                                    [config.M, -1, config.num_classes])
